180cb4223b6ec730e55d937c3e9981ea2f2bc883
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-03-08 12:29:16 -0700

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { LibraryPage } from '../pages/library/library';
+import { CategoryPage } from '../pages/category/category';
+import { QuotePage } from '../pages/quote/quote';
+import { FavoritesPage } from '../pages/favorites/favorites';
+import { SettingsPage } from '../pages/settings/settings';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+  declarations: [
+    MyApp,
+	LibraryPage,
+	CategoryPage,
+	QuotePage,
+	FavoritesPage,
+	SettingsPage,
+	TabsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+	LibraryPage,
+	CategoryPage,
+	QuotePage,
+	FavoritesPage,
+	SettingsPage,
+	TabsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/pages/library/library.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+
+import quotes from '../../data/quotes';
+/*
+  Generated class for the Library page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-library',
+  templateUrl: 'library.html'
+})
+export class LibraryPage implements OnInit {
+	quoteCollection: {category: string, quotes: Quote[], icon: string}[];
+  
+	constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+	ngOnInit(){
+		this.quoteCollection = quotes;
+	}
+}
